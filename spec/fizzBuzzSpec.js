4171bc6b957df8f61c869b7ee73a34897b39649d
George Eaton <george_eaton@hotmail.co.uk>
2016-12-06 13:27:56 +0000

    first commit

---
+++ spec/fizzBuzzSpec.js
@@ -0,0 +1,12 @@
+function FizzBuzz(number) {
+
+if fizzbuzz.isDivisibleBy3
+  return buzz
+else if fizzbuzz.isDivisibleBy5
+  return fizz
+  else if fizzbuzz.isDivisibleBy3and5
+  else
+  return number
+
+
+}
